7deeef6d70e9e54369891a57d83b441f9eaa2256
root <michaeltekle1571@gmail.com>
2021-08-28 22:29:37 +0300

    Update casuality.py

--- scripts/casuality.py
+++ scripts/casuality.py
@@ -62,7 +62,7 @@ class casuality:
         similarity = []
         sm_total = [sm]
         count = 0
-        cor = {0:9, 1:8, 2:7, 3:6, 4:5, 5:4, 6:3, 7:2, 8:1}
+        cor = {0:3, 1:2, 2:1}
 
         for i in range(3, 0, -1):
                 
